eee50ce712b6df8103afa94d872ac75da36e61b3
jExbrayat <jules.exbrayat@gmail.com>
2023-12-22 18:35:33 +0100

    feat: implement sphinx until Makefile

---
+++ docs/source/conf.py
@@ -0,0 +1,37 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "recognize-handwritten-letters"
+copyright = "2023, Exbrayat Madani"
+author = "Exbrayat Madani"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+import sys
+from pathlib import Path
+
+## Add root folder to path
+filepath = Path(__file__).resolve()
+root_folder = str(filepath.parent.parent.parent)
+sys.path.append(root_folder)
+sys.path.append(f"{root_folder}/src")
+
+
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.viewcode", "sphinx.ext.napoleon"]
+
+
+templates_path = ["_templates"]
+exclude_patterns = []
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
---
+++ docs/source/index.rst
@@ -0,0 +1,21 @@
+.. recognize-handwritten-letters documentation master file, created by
+   sphinx-quickstart on Fri Dec 22 17:59:24 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to recognize-handwritten-letters's documentation!
+=========================================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   modules.rst
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
