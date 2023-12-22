f228640b7cf6dbb38ccea02d7bc87a091d745f31
jExbrayat <jules.exbrayat@gmail.com>
2023-12-22 02:21:43 +0100

    feat: define plot training curve function

---
+++ src/plot_training_curve.py
@@ -0,0 +1,43 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def plot_training_curve(
+    training_loss: list,
+    validation_loss: list,
+    plot_from_n_epoch: int,
+    validation_accuracy: list = None,
+):
+    """Plot training curves of a keras model (train loss, val loss, val accuracy if classification model)
+
+    Args:
+        training_loss (list): training loss gathered from history callback
+        validation_loss (list): validation loss gathered from history callback
+        validation_accuracy (list): validation accuracy gathered from history callback
+        plot_from_n_epoch (int): epoch from which to plot when there are too many
+    """
+
+    num_epochs_to_display = len(training_loss) - plot_from_n_epoch
+    step_x_ticks = (
+        int(num_epochs_to_display / 10) if int(num_epochs_to_display / 10) >= 1 else 1
+    )
+
+    plt.plot(training_loss[plot_from_n_epoch:], label="training loss")
+    plt.plot(validation_loss[plot_from_n_epoch:], label="validation loss")
+
+    if validation_accuracy != None:
+        plt.plot(validation_accuracy[plot_from_n_epoch:], label="validation accuracy")
+
+    plt.legend()
+    plt.title("training curve")
+
+    plt.xticks(
+        np.arange(0, num_epochs_to_display, step=step_x_ticks),
+        np.arange(
+            plot_from_n_epoch + 1,
+            plot_from_n_epoch + num_epochs_to_display + 1,
+            step=step_x_ticks,
+        ),
+    )
+
+    plt.show()
