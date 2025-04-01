9bca55ff4b400ea9be6c3f98aacd3f0b8ba3a754
Ab2nour <61651582+Ab2nour@users.noreply.github.com>
2025-04-02 00:35:32 +0200

    refactor: improve data loading functions

--- src/constants.py
+++
@@ -1,3 +0,0 @@
-data_folder = "data"
-img_shape = 28, 28
-nb_classes = 26
--- src/data/load_data.py
+++ src/data/load_data.py
@@ -1,8 +1,36 @@
+from typing import Tuple
+
+import numpy as np
 import pandas as pd
 
-from src.constants import data_folder
+from src.utils.constants import data_folder
 
 
 def load_sample_data() -> pd.DataFrame:
+    """Load sample data.
+
+    >>> df = load_sample_data()
+    """
     df = pd.read_csv(f"{data_folder}/raw/sample-dataset.csv", header=None)
     return df
+
+
+def get_x_y(df: pd.DataFrame) -> Tuple[np.ndarray, np.ndarray]:
+    """Get x and y, where x is the input data and y the labels to predict.
+
+    >>> df = load_sample_data()
+    >>> x, y = get_x_y(df)
+    """
+    x: np.ndarray = df.iloc[:, 1:].to_numpy()
+    y: np.ndarray = df.iloc[:, 0].to_numpy()
+    return x, y
+
+
+def load_sample_x_y() -> Tuple[np.ndarray, np.ndarray]:
+    """Load sample x and y.
+
+    >>> x, y = load_sample_x_y()"""
+    df = load_sample_data()
+    x, y = get_x_y(df)
+
+    return x, y
