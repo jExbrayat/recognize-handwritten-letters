c58ff5ac91e412f0c00f332e34f1b55aa1ddc340
Ab2nour <61651582+Ab2nour@users.noreply.github.com>
2025-04-02 03:41:35 +0200

    refactor: various py files

--- src/data/load_data.py
+++ src/data/load_data.py
@@ -1,7 +1,6 @@
-from typing import Tuple
-
 import numpy as np
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
 from src.utils.constants import data_folder
 
@@ -11,11 +10,11 @@ def load_sample_data() -> pd.DataFrame:
 
     >>> df = load_sample_data()
     """
-    df = pd.read_csv(f"{data_folder}/raw/sample-dataset.csv", header=None)
+    df = pd.read_parquet(f"{data_folder}/raw/sample-uniform.parquet", engine="pyarrow")
     return df
 
 
-def get_x_y(df: pd.DataFrame) -> Tuple[np.ndarray, np.ndarray]:
+def get_x_y(df: pd.DataFrame) -> tuple[np.ndarray, np.ndarray]:
     """Get x and y, where x is the input data and y the labels to predict.
 
     >>> df = load_sample_data()
@@ -26,11 +25,33 @@ def get_x_y(df: pd.DataFrame) -> Tuple[np.ndarray, np.ndarray]:
     return x, y
 
 
-def load_sample_x_y() -> Tuple[np.ndarray, np.ndarray]:
+def load_sample_x_y() -> tuple[np.ndarray, np.ndarray]:
     """Load sample x and y.
 
-    >>> x, y = load_sample_x_y()"""
+    >>> x, y = load_sample_x_y()
+    """
     df = load_sample_data()
     x, y = get_x_y(df)
 
     return x, y
+
+
+def load_sample_train_test(
+    test_size: float = 0.2, random_state: int = 0
+) -> tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+    """Load sample train test.
+
+    >>> x_train, x_test, y_train, y_test = load_sample_train_test()
+    """
+    x, y = load_sample_x_y()
+
+    x_train, x_test, y_train, y_test = train_test_split(
+        x, y, stratify=y, random_state=random_state, test_size=test_size
+    )
+
+    x_train = np.array(x_train)
+    y_train = np.array(y_train)
+    x_test = np.array(x_test)
+    y_test = np.array(y_test)
+
+    return x_train, x_test, y_train, y_test
---
+++ src/data/preprocessing.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+
+def preprocess(x: np.ndarray) -> np.ndarray:
+    return x / 255.0
